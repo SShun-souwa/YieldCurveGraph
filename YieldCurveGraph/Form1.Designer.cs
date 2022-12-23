7dd247c79be30c6c38b64b21fa34abc9bde3dd3c
SShun <windwouwa@yahoo.co.jp>
2022-12-24 03:43:55 +0900

    プロジェクト ファイルを追加します。

---
+++ YieldCurveGraph/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace YieldCurveGraph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.YieldCurve = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.StockInd = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.play = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labeldate = new System.Windows.Forms.Label();
+            this.DateTime = new System.Windows.Forms.Label();
+            this.highest = new System.Windows.Forms.Label();
+            this.fall = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.startday = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateerror = new System.Windows.Forms.Label();
+            this.DJIbutton = new System.Windows.Forms.Button();
+            this.SP500button = new System.Windows.Forms.Button();
+            this.NDX100button = new System.Windows.Forms.Button();
+            this.IndName = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.YieldCurve)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StockInd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // YieldCurve
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.YieldCurve.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.YieldCurve.Legends.Add(legend1);
+            this.YieldCurve.Location = new System.Drawing.Point(25, 100);
+            this.YieldCurve.Name = "YieldCurve";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.YieldCurve.Series.Add(series1);
+            this.YieldCurve.Size = new System.Drawing.Size(750, 500);
+            this.YieldCurve.TabIndex = 0;
+            this.YieldCurve.Text = "chart1";
+            // 
+            // StockInd
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.StockInd.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.StockInd.Legends.Add(legend2);
+            this.StockInd.Location = new System.Drawing.Point(800, 100);
+            this.StockInd.Name = "StockInd";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.StockInd.Series.Add(series2);
+            this.StockInd.Size = new System.Drawing.Size(750, 500);
+            this.StockInd.TabIndex = 1;
+            this.StockInd.Text = "chart2";
+            // 
+            // play
+            // 
+            this.play.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.play.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.play.Location = new System.Drawing.Point(850, 630);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(200, 100);
+            this.play.TabIndex = 2;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // stop
+            // 
+            this.stop.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.stop.Location = new System.Drawing.Point(1075, 630);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(200, 100);
+            this.stop.TabIndex = 3;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // reset
+            // 
+            this.reset.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.reset.Location = new System.Drawing.Point(1300, 630);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(200, 100);
+            this.reset.TabIndex = 4;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(950, 751);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 19);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "1000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.label1.Location = new System.Drawing.Point(867, 753);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 14);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "表示スピード";
+            // 
+            // labeldate
+            // 
+            this.labeldate.AutoSize = true;
+            this.labeldate.Font = new System.Drawing.Font("MS UI Gothic", 100F);
+            this.labeldate.Location = new System.Drawing.Point(25, 685);
+            this.labeldate.Name = "labeldate";
+            this.labeldate.Size = new System.Drawing.Size(775, 134);
+            this.labeldate.TabIndex = 7;
+            this.labeldate.Text = "yyyy-mm-dd";
+            // 
+            // DateTime
+            // 
+            this.DateTime.AutoSize = true;
+            this.DateTime.Font = new System.Drawing.Font("MS UI Gothic", 40F);
+            this.DateTime.Location = new System.Drawing.Point(25, 630);
+            this.DateTime.Name = "DateTime";
+            this.DateTime.Size = new System.Drawing.Size(240, 54);
+            this.DateTime.TabIndex = 8;
+            this.DateTime.Text = "DateTime";
+            // 
+            // highest
+            // 
+            this.highest.AutoSize = true;
+            this.highest.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.highest.Location = new System.Drawing.Point(1125, 50);
+            this.highest.Name = "highest";
+            this.highest.Size = new System.Drawing.Size(104, 40);
+            this.highest.TabIndex = 9;
+            this.highest.Text = "Point";
+            // 
+            // fall
+            // 
+            this.fall.AutoSize = true;
+            this.fall.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.fall.Location = new System.Drawing.Point(1375, 50);
+            this.fall.Name = "fall";
+            this.fall.Size = new System.Drawing.Size(150, 40);
+            this.fall.TabIndex = 10;
+            this.fall.Text = "Percent";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label2.Location = new System.Drawing.Point(1100, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "史上最高値";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label3.Location = new System.Drawing.Point(1300, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(245, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "史上最高値後の最大下落率";
+            // 
+            // startday
+            // 
+            this.startday.Location = new System.Drawing.Point(904, 814);
+            this.startday.Name = "startday";
+            this.startday.Size = new System.Drawing.Size(100, 19);
+            this.startday.TabIndex = 13;
+            this.startday.Text = "yyyy/mm/dd";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.label4.Location = new System.Drawing.Point(867, 788);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 14);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "途中の日時から表示";
+            // 
+            // dateerror
+            // 
+            this.dateerror.AutoSize = true;
+            this.dateerror.Font = new System.Drawing.Font("MS UI Gothic", 10F);
+            this.dateerror.ForeColor = System.Drawing.Color.Red;
+            this.dateerror.Location = new System.Drawing.Point(909, 840);
+            this.dateerror.Name = "dateerror";
+            this.dateerror.Size = new System.Drawing.Size(0, 14);
+            this.dateerror.TabIndex = 15;
+            // 
+            // DJIbutton
+            // 
+            this.DJIbutton.Location = new System.Drawing.Point(1100, 750);
+            this.DJIbutton.Name = "DJIbutton";
+            this.DJIbutton.Size = new System.Drawing.Size(113, 56);
+            this.DJIbutton.TabIndex = 16;
+            this.DJIbutton.Text = "DJI";
+            this.DJIbutton.UseVisualStyleBackColor = true;
+            this.DJIbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SP500button
+            // 
+            this.SP500button.Location = new System.Drawing.Point(1225, 750);
+            this.SP500button.Name = "SP500button";
+            this.SP500button.Size = new System.Drawing.Size(113, 56);
+            this.SP500button.TabIndex = 17;
+            this.SP500button.Text = "SP500";
+            this.SP500button.UseVisualStyleBackColor = true;
+            this.SP500button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // NDX100button
+            // 
+            this.NDX100button.Location = new System.Drawing.Point(1350, 750);
+            this.NDX100button.Name = "NDX100button";
+            this.NDX100button.Size = new System.Drawing.Size(113, 56);
+            this.NDX100button.TabIndex = 18;
+            this.NDX100button.Text = "NDX100";
+            this.NDX100button.UseVisualStyleBackColor = true;
+            this.NDX100button.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // IndName
+            // 
+            this.IndName.AutoSize = true;
+            this.IndName.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.IndName.Location = new System.Drawing.Point(900, 50);
+            this.IndName.Name = "IndName";
+            this.IndName.Size = new System.Drawing.Size(75, 40);
+            this.IndName.TabIndex = 19;
+            this.IndName.Text = "DJI";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.label5.Location = new System.Drawing.Point(845, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(205, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "現在表示中の株価指数";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.IndName);
+            this.Controls.Add(this.NDX100button);
+            this.Controls.Add(this.SP500button);
+            this.Controls.Add(this.DJIbutton);
+            this.Controls.Add(this.dateerror);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.startday);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fall);
+            this.Controls.Add(this.highest);
+            this.Controls.Add(this.DateTime);
+            this.Controls.Add(this.labeldate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.StockInd);
+            this.Controls.Add(this.YieldCurve);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.YieldCurve)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.StockInd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart YieldCurve;
+        private System.Windows.Forms.DataVisualization.Charting.Chart StockInd;
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labeldate;
+        private System.Windows.Forms.Label DateTime;
+        private System.Windows.Forms.Label highest;
+        private System.Windows.Forms.Label fall;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox startday;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label dateerror;
+        private System.Windows.Forms.Button DJIbutton;
+        private System.Windows.Forms.Button SP500button;
+        private System.Windows.Forms.Button NDX100button;
+        private System.Windows.Forms.Label IndName;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ YieldCurveGraph/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace YieldCurveGraph
+{
+    public partial class Form1 : Form
+    {   
+        // Form1クラスで利用する変数の定義
+        List<string[]> lists = new List<string[]>();
+        string[] yieldX = { "3M", "6M", "1Y", "2Y", "3Y", "5Y", "7Y", "10Y", "20Y", "30Y" };
+        // グラフの表示日数のカウント
+        int count = 0;
+        // 史上最高値日が格納されたリストのインデックス番号を格納
+        int highcount = 0;
+        // グラフの再生、停止の判定
+        bool playjudge = false;
+        // グラフの更新スピード
+        int speed = 1000;
+        // 史上最高値、最高値後の最安値、下落率を格納するリスト
+        double[] maxmin = { 1.0, 1.0, 1.0 };
+        // 表示する株価指数
+        string stind = "DJI";
+
+        // イールドカーブのグラフエリアをセットするメソッド
+        public void YieldcurveChart()
+        {
+            // チャートエリアの初期化
+            YieldCurve.ChartAreas.Clear();
+            YieldCurve.Series.Clear();
+
+            // ｙ軸上限値、下限値設定に利用するリストの定義
+            var yieldY = new List<double>();
+
+            // チャートエリアを生成し、Chartコントロールに追加
+            ChartArea chartAria = new ChartArea("chartArea");
+            YieldCurve.ChartAreas.Add(chartAria);
+
+            // Series(系列)を生成
+            Series series = new Series();
+
+            // 系列の種類を折れ線グラフに指定
+            series.ChartType = SeriesChartType.Line;
+
+            // 系列の凡例を非表示
+            series.IsVisibleInLegend = false;
+
+            // グラフの設定
+            series.Color = ColorTranslator.FromHtml("#32aed4");  //グラフ色
+            series.BorderWidth = 3;                              //折れ線の幅
+            series.MarkerSize = 10;                              //マーカーのサイズ
+            series.MarkerColor = Color.Blue;                     //マーカーの背景色
+            series.MarkerBorderColor = Color.Black;              //マーカーの枠の色
+            series.MarkerStyle = MarkerStyle.Square;
+
+            // 系列のポイント情報をセット、イールドカーブのデータは１０種（CSVファイルの２～１０列）
+            for (int i = 0; i < 10; i++)
+            {   
+                // Seriesにデータを追加
+                series.Points.AddXY(yieldX[i], lists[count][i + 1]);
+                // y軸設定用のリストにもデータを追加
+                yieldY.Add(double.Parse(lists[count][i + 1])); ;
+            }
+
+            // y軸の設定
+            Axis axisY = new Axis();
+            // y軸の上端を10%、下端は20%の幅で表示
+            axisY.Minimum = Math.Round(0.8 * yieldY.Min(), 1, MidpointRounding.AwayFromZero);
+            axisY.Maximum = Math.Round(1.1 * yieldY.Max(), 1, MidpointRounding.AwayFromZero);
+            YieldCurve.ChartAreas[0].AxisY = axisY;
+
+            // 生成・設定した系列をChartコントロールに追加
+            YieldCurve.Series.Add(series);
+
+        }
+
+        // 株価指数のグラフエリアをセットするメソッド
+        public void StockIndChart()
+        {
+            // チャートエリアの初期化
+            StockInd.ChartAreas.Clear();
+            StockInd.Series.Clear();
+
+            // チャートエリアを生成し、Chartコントロールに追加
+            ChartArea chartAria1 = new ChartArea("chartArea1");
+            StockInd.ChartAreas.Add(chartAria1);
+
+            // Series(系列)を生成
+            Series series1 = new Series();
+
+            // 系列の種類を折れ線グラフに指定
+            series1.ChartType = SeriesChartType.Line;
+
+            // 系列の凡例を非表示
+            series1.IsVisibleInLegend = false;
+
+            // グラフの設定
+            series1.Color = ColorTranslator.FromHtml("#dc143c");  //グラフ色の設定
+            series1.BorderWidth = 3;                              //ボーダーの幅            
+
+            // 系列のポイント情報をセット、イールドカーブのデータは１０種（CSVファイルの１１列）
+            for (int j = 1; j < count; j++)
+            {   
+                // シリーズに終値データを追加
+                series1.Points.AddXY(lists[j][0], lists[j][11]);
+
+                // 史上最高値を撮った場合と、最高値をとった後の最安値までの下落率を計算し、リストに格納
+                if (maxmin[0] < double.Parse(lists[j][11]))
+                {
+                    maxmin[0] = double.Parse(lists[j][11]);
+                    maxmin[1] = double.Parse(lists[j][11]);
+                    highest.Text = maxmin[0].ToString();
+                    highcount = count;
+
+                }
+                else if (maxmin[1] > double.Parse(lists[j][11]) & j > highcount)
+                {
+                    maxmin[1] = double.Parse(lists[j][11]);
+                    maxmin[2] = Math.Round((1 - maxmin[1] / maxmin[0]) * 100, 2, MidpointRounding.AwayFromZero);
+                    fall.Text = maxmin[2].ToString() + "%";
+                }
+            }
+            // 生成・設定した系列をChartコントロールに追加
+            StockInd.Series.Add(series1);
+        }
+
+        public void setIndex()
+        {
+            // データセット（CSV）の読み込み
+            string fileName = "I:\\HandMadePrograms\\YieldCurveGraph\\YieldCurveGraph\\YieldCurveGraph\\DataSet\\" + stind + "data.csv";
+            StreamReader sr = new StreamReader(@fileName);
+            int i = 0;
+
+            //読み込んだファイルを1行ずつ処理
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                if (i == 0)
+                {
+                    //1行目はヘッダーとして別途取得
+                    string[] cols = line.Split(',');
+                    i += 1;
+                    continue;
+                }
+
+                //カンマで配列の要素に分割
+                string[] values = line.Split(',');
+
+                // 配列からリストに格納
+                lists.Add(values);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            setIndex();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 再生ボタンを押下した時の処理（非同期宣言）
+        private async void play_Click(object sender, EventArgs e)
+        {
+            // 現在表示している株価指数名の表示
+            IndName.Text = stind;
+            // 日付のエラー文の初期化
+            dateerror.Text = "";
+            playjudge = true;
+            // CSVを読み込んだリストをループ処理
+            while (count < lists.Count)
+            {
+                // speedの値が空欄もしくは0の場合に1000を代入
+                if(textBox1.Text == ""　|| textBox1.Text == "0")
+                {
+                    speed = 1000;
+                }
+
+                // 表示スピードを入力された値(int型)に更新
+                speed = int.Parse(textBox1.Text);
+                // 再生判定がTrueの間、リストの先頭からグラフに表示
+                if (playjudge)
+                {
+                    // グラフの表示
+                    YieldcurveChart();
+                    StockIndChart();
+
+                    // 表示しているデータの日付の表示を更新
+                    labeldate.Text = lists[count][0];
+                    count += 1;
+
+                    // 次のグラフの表示までのウェイト処理
+                    await Task.Delay(10000/speed);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        // 再生ストップボタンを押下した時の処理
+        private void stop_Click(object sender, EventArgs e)
+        {   
+            // 再生の一時停止判定
+            playjudge = false;
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {   
+            // 最高値と下落率の表示のリセット
+            highest.Text = "";
+            fall.Text = "";
+            // 再生の一時停止判定
+            playjudge = false;
+            // 表示期間指定を指定した時の処理
+            if(startday.Text == "0" || startday.Text =="yyyy/mm/dd" || startday.Text == "")
+            {
+                count = 0;
+            }
+            else{
+                int i = 0;
+                bool test = true;
+                // CSVデータの日付カラムから入力された日付と同日のインデックス番号を検索し、countに代入する処理
+                while(i < lists.Count) 
+                {
+                    if(startday.Text == lists[i][0]) 
+                    {
+                        dateerror.Text = "";    // 日付エラーテキストの非表示
+                        count = i;              // countへ代入
+                        test = false;           // エラー出力の可否をfalseに
+                        maxmin[0] = 1.0;
+                        break;
+                    }
+
+                    i++;
+                }
+                // 指定された日付がデータに存在しない場合にエラーの表示
+                if (test)
+                {
+                    dateerror.Text = "指定された日付のデータはありません。日時をずらしてください。";
+                }
+            }
+
+        }
+
+        // 表示する株価指数の切り替え
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stind = "DJI";
+            // データを格納していたリストと、史上最高値、最高値後の最安値の値をリセット
+            lists.Clear();
+            maxmin[0] = 1.0;
+            maxmin[1] = 1.0;
+            // 対応する株価指数のデータを再度CSVから読み込み
+            setIndex();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stind = "SP500";
+            // データを格納していたリストと、史上最高値、最高値後の最安値の値をリセット
+            lists.Clear();
+            maxmin[0] = 1.0;
+            maxmin[1] = 1.0;
+            // 対応する株価指数のデータを再度CSVから読み込み
+            setIndex();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stind = "NDX100";
+            // データを格納していたリストと、史上最高値、最高値後の最安値の値をリセット
+            lists.Clear();
+            maxmin[0] = 1.0;
+            maxmin[1] = 1.0;
+            // 対応する株価指数のデータを再度CSVから読み込み
+            setIndex();
+        }
+    }
+}
